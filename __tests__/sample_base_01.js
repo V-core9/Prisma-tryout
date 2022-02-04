7dee1dd7033e6774586bce12af8e0947bd5d5872
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-02-04 18:46:07 +0100

    yea passing 2k test count...brb gonna run to store xD

--- __tests__/sample_base_01.js
+++ __tests__/sample_base_01.js
@@ -1,18 +1,43 @@
-const {users, posts} = require('../models');
+const { users, posts } = require('../models');
 
 var faker = require('faker');
+
+//! Test Configs:
+const item_count = 1000;
+
+
+var generator = {
+  newUser: async () => {
+    return { email: faker.internet.email(), password: faker.internet.password(), username: faker.internet.userName(), salt: '123$@$' };
+  },
+  newPost: async () => {
+    return {  title: faker.lorem.sentence(), content: faker.lorem.paragraphs(), published: false  };
+  },
+};
 
 afterAll(async () => {
   await users.purge();
   await posts.purge();
 });
 
-test('adding user #1', async () => {
-  const testUser = { email: faker.internet.email(), password: faker.internet.password(), username: faker.internet.userName(), salt: 'aoiobo$@$'};
-  const result = await users.new(testUser);
-  testUser.id = result.id;
-  expect(result).toStrictEqual(testUser);
-});
+for (var i = 0; i < item_count; i++) {
+  test('adding user #' + i, async () => {
+    var user = await generator.newUser();
+    const result = await users.new(user);
+    user.id = result.id;
+    expect(result).toStrictEqual(user);
+  });
+}
 
 
 
+
+for (var i = 0; i < item_count; i++) {
+  test('adding posts #' + i, async () => {
+    var post = await generator.newPost();
+    const result = await posts.new(post);
+    post.id = result.id;
+    post.authorId = null;
+    expect(result).toStrictEqual(post);
+  });
+}
