051cfb36ece92ab1a7f1dd963f570f815e4855bb
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-02-12 16:58:07 +0100

    ok;

--- source/Examples/01.Express.cluster.js/actions.js
+++ source/Examples/01.Express.cluster.js/actions.js
@@ -12,15 +12,15 @@ v9_stringify = async (data) => {
 };
 
 const api = {
-  root: async (req, res) => res.end(await v9_stringify({ timestamp: Date.now(), models: v9_models, coreCore: 'Vc9_initPrisma', version: '1.0.0' }), 'utf-8'),
-  type: async (req, res) => res.end(await v9_stringify(await model_exists(req.params.type) ? await prisma[req.params.type].findMany({ take: 5 }) : {}), 'utf-8'),
-  typeBySlug: async (req, res) => res.end(await v9_stringify(await model_exists(req.params.type) ? await prisma[req.params.type].findUnique({ where: { slug: req.params.slug } }) : {}), 'utf-8'),
-  userByUsername: async (req, res) => res.end(await v9_stringify(await prisma.user.findUnique({ where: { username: req.params.username } })), 'utf-8'),
+  root: async (req, res) => res.end(await v9_stringify({ timestamp: Date.now(), models: v9_models, coreCore: 'Vc9_initPrisma', version: '1.0.0' })),
+  type: async (req, res) => res.end(await v9_stringify(await model_exists(req.params.type) ? await prisma[req.params.type].findMany({ take: 5 }) : {})),
+  typeBySlug: async (req, res) => res.end(await v9_stringify(await model_exists(req.params.type) ? await prisma[req.params.type].findUnique({ where: { slug: req.params.slug } }) : {})),
+  userByUsername: async (req, res) => res.end(await v9_stringify(await prisma.user.findUnique({ where: { username: req.params.username } }))),
 };
 
 const app = {
   homepage: async (req, res) => res.end("Hello!"),
-  pageBySlug: async (req, res) => res.end(await v9_stringify(await prisma.page.findUnique({ where: { slug: req.params.slug } })), 'utf-8'),
+  pageBySlug: async (req, res) => res.end(await v9_stringify(await prisma.page.findUnique({ where: { slug: req.params.slug } }))),
 };
 
-module.exports = {api, app};
+module.exports = { api, app };
